31ea035aa72dc707e69978f52e7a44f38ff2e4ba
Oussama <oussamaelarif@outlook.com>
2021-08-18 16:19:18 +0100

    Update to script for transforming data

--- Extract.py
+++ Extract.py
@@ -1,4 +1,3 @@
-import csv
 import pandas as pd
 
 
@@ -6,14 +5,5 @@ def extract_csv(file_name):
     df = pd.read_csv(file_name)
     print(df)
     return df
-
-
-# def read_csv_file(file_name, csv_to_read):
-#     with open(file_name, 'r') as csv_file:
-#         csv_to_read = csv.DictReader(csv_file)
-#         csv_list = []
-#         for row in csv_to_read:
-#             csv_list.append(row)
-#         return csv_list
 
 extract_csv('2021-02-23-isle-of-wight.csv')
--- transform.py
+++ transform.py
@@ -1,9 +1,9 @@
-import csv
 import pandas as pd
 
 column_names = ["DateTime", "Location", "Customer_Name",
                 "Order", "Payment_Method", "Total_Price", "PII"]
-df = pd.read_csv("2021-02-23-isle-of-wight.csv", names=column_names)
+df = pd.read_csv("team-2-project/2021-02-23-isle-of-wight.csv",
+                 names=column_names)
 
 
 for value in df["PII"]:
@@ -26,6 +26,42 @@ to_drop = ["Customer_Name", "PII", "DateTime",
            "Account_number"]  # Data to sanitize
 
 
+# for row in df["Orders"]:
+
+
+# print(unique_products)
+
+
 # inplace whether the returned data is copied(False) or updated(True)
 delete_data = df.drop(columns=to_drop, inplace=False)
-print(delete_data)
+# print(delete_data.head())
+
+
+#########################
+# Quick test to insert values into empty csv spaces
+
+orders = df["Order"]
+
+print(orders)
+# new_orders = []
+
+for order in orders:
+    # print(order)
+    indexes = order.split(",")
+
+
+print(indexes)
+
+for place, item in enumerate(indexes):
+    if item == "":
+        indexes[place] = "Regular"
+
+print(indexes)
+#     new_orders.append(item)
+
+# print(f"new {indexes}")
+
+# print(len(indexes))
+
+# print(new_orders)
+# product_dict = {"Product_ID" : range(1, len(new_orders[0]), "Product_name" : new_order)}
