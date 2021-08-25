70f603014a57e53909df3137642f9485eaa1ed4e
jaltpi <jenielthomas@gmail.com>
2021-08-25 11:53:27 +0100

    Created function pandas to sql, and created a function to create a database connection.

--- Load.py
+++ Load.py
@@ -27,3 +27,27 @@ from sqlalchemy import create_engine
 
 # Pandas DataFrame Payment -> SQl
 # payments_df.to_sql("Payments", engine, if_exists ="replace")
+
+def pandas_to_sql(dataframe: object, table_name: str, connection, schema = None, if_exists = "replace", index = True, index_label = None, chunksize = None, dtype = None, method = None):
+    """"This function allows the user to take a dataframe and moves its data into a SQL database"""
+    try:
+        dataframe.to_sql(table_name, connection, schema, if_exists, index, index_label, chunksize, dtype)
+    except Exception as e:
+        print(f"Error: {e}.")
+        return "Fail"
+    else:
+        return "Success"
+        
+def create_connection(system: str,user_name: str, password: str, host: str, port: str, database_name: str):
+    """"This function allows the user the create an engine using SQLAlchemy. If successful, it returns the connection."""
+    try:
+        print("Establishing connection too database...")
+        engine = create_engine(f"{system}://{user_name}:{password}@{host}:{port}/{database_name}")
+    
+    except Exception as e:
+        print(f"Error: {e}. Failure to connect to database.")
+        return "Fail"
+    
+    else:
+        print("Successful connection to database established.")
+        return engine
