57d66a3cac8d6a4cee44a776bab33abe2366a074
jaltpi <jenielthomas@gmail.com>
2021-09-14 10:24:36 +0100

    Removed previous transform function, and moved extract function into Lambda Handler Function

--- src/team-2-ETL/handler.py
+++ src/team-2-ETL/handler.py
@@ -3,22 +3,36 @@ import pandas as pd
 import io
 import boto3
 import csv
-
-def extract_file(pandas_library : object) -> object:
-    column = ["datetime", "location", "name", "products",
-            'total_price', "payment_type", "card_number"]
-    
-    # Get key and bucket information
-    key = event['Records'][0]['s3']['object']['key']
-    bucket = event['Records'][0]['s3']['bucket']['name']
+# def extract_file() -> object:
+#     column = ["datetime", "location", "name", "products",
+#                 'total_price', "payment_type", "card_number"]
+        
+#     # Get key and bucket information
+#     key = event['Records'][0]['s3']['object']['key']
+#     bucket = event['Records'][0]['s3']['bucket']['name']
             
             
-    s3 = boto3.client('s3')
-    obj = s3.get_object(Bucket= bucket, Key= key)
-    raw_df = pd.read_csv(io.BytesIO(obj['Body'].read()), names = column)
-    return raw_df
+#     s3 = boto3.client('s3')
+#     obj = s3.get_object(Bucket= bucket, Key= key)
+#     raw_df = pd.read_csv(io.BytesIO(obj['Body'].read()), names = column)
+#     return raw_df
 
 def ETLPipeline(event, context):
+    def extract_file() -> object:
+        
+        column = ["datetime", "location", "name", "products",
+                'total_price', "payment_type", "card_number"]
+        
+        # Get key and bucket information
+        key = event['Records'][0]['s3']['object']['key']
+        bucket = event['Records'][0]['s3']['bucket']['name']
+                
+                
+        s3 = boto3.client('s3')
+        obj = s3.get_object(Bucket= bucket, Key= key)
+        raw_df = pd.read_csv(io.BytesIO(obj['Body'].read()), names = column)
+        
+        return raw_df
     body = {
         "message": "Go Serverless v2.0! Your function executed successfully!",
         "input": event,
