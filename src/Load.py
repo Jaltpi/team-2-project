dfbe3ae645fd38ca9b83896e8bb0e87042970ca3
jaltpi <jenielthomas@gmail.com>
2021-09-14 15:53:05 +0100

    Added parathesis around 'Items' variable in data_to_load function

--- src/Load.py
+++ src/Load.py
@@ -118,7 +118,7 @@ def load_data_redshift(table: str, columns: str, data: str, Items: list):
         cursor = conn.cursor()
         
         # Insert Data into table
-        cursor.executemany(f"""INSERT INTO {table} ({columns}) VALUES ({data})""", Items)
+        cursor.executemany(f"""INSERT INTO {table} ({columns}) VALUES ({data})""", (Items))
         print("Command executed successfully.")
         conn.commit()
         conn.close()
