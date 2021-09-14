1b4d543a06fdecf7b08cfeffdc2fcbbc5194abb0
jaltpi <jenielthomas@gmail.com>
2021-09-14 10:22:38 +0100

    Commented out previous code, added in function to load to database redshift

--- src/Load.py
+++ src/Load.py
@@ -1,6 +1,7 @@
 import psycopg2
-from sqlalchemy import create_engine
-from config import config
+import boto3
+# from sqlalchemy import create_engine
+# from config import config
 
 
 # TODO = ["Create database", "Create tables in database", "Upload data into tables"]
@@ -30,59 +31,98 @@ from config import config
 # Pandas DataFrame Payment -> SQl
 # payments_df.to_sql("Payments", engine, if_exists ="replace")
 
-def pandas_to_sql(dataframe: object, table_name: str, connection, schema=None, if_exists="replace", index=False, index_label=None, chunksize=None, dtype=None, method=None):
-    """"This function allows the user to take a dataframe and moves its data into a SQL database"""
+# def pandas_to_sql(dataframe: object, table_name: str, connection, schema=None, if_exists="replace", index=False, index_label=None, chunksize=None, dtype=None, method=None):
+#     """"This function allows the user to take a dataframe and moves its data into a SQL database"""
+#     try:
+#         dataframe.to_sql(table_name, connection, schema,
+#                          if_exists, index, index_label, chunksize, dtype)
+#     except Exception as e:
+#         print(f"Error: {e}.")
+#         return "Fail"
+#     else:
+#         return "Success"
+
+
+# def create_connection(system: str, user_name: str, password: str, host: str, port: str, database_name: str):
+#     """"This function allows the user the create an engine using SQLAlchemy. If successful, it returns the connection."""
+#     try:
+#         print("Establishing connection too database...")
+#         engine = create_engine(
+#             f"{system}://{user_name}:{password}@{host}:{port}/{database_name}")
+
+#     except Exception as e:
+#         print(f"Error: {e}. Failure to connect to database.")
+#         return "Fail"
+
+#     else:
+#         print("Successful connection to database established.")
+#         return engine
+
+
+# def SQL_INSERT_STATEMENT_FROM_DATAFRAME(SOURCE, TARGET):
+#     sql_texts = []
+#     for index, row in SOURCE.iterrows():
+#         sql_texts.append('INSERT INTO '+TARGET+' (' +
+#                          str(', '.join(SOURCE.columns)) + ') VALUES ' + str(tuple(row.values)))
+#     return sql_texts
+
+
+# def SQL_commands_executor(commands: list):
+#     conn = None
+#     try:
+#         # read the connection parameters
+#         params = config()
+#         # connect to the PostgreSQL server
+#         conn = psycopg2.connect(**params)
+#         cur = conn.cursor()
+#         # create command one by one
+#         for command in commands:
+#             cur.execute(command)
+#         # close communication with the PostgreSQL database server
+#         cur.close()
+#         # commit the changes
+#         conn.commit()
+#     except (Exception, psycopg2.DatabaseError) as error:
+#         print(error)
+#     finally:
+#         if conn is not None:
+#             conn.close()
+def load_data_redshift(table: str, columns: str, Items: list):
+    """This function connects to the the redshift database and inserts a list of tuples if the connection was successful.
+    User must provide the table name (string), and which columns (string without parenthesis enclosing the column names)
+    to insert the data into"""
     try:
-        dataframe.to_sql(table_name, connection, schema,
-                         if_exists, index, index_label, chunksize, dtype)
-    except Exception as e:
-        print(f"Error: {e}.")
-        return "Fail"
-    else:
-        return "Success"
-
-
-def create_connection(system: str, user_name: str, password: str, host: str, port: str, database_name: str):
-    """"This function allows the user the create an engine using SQLAlchemy. If successful, it returns the connection."""
-    try:
-        print("Establishing connection too database...")
-        engine = create_engine(
-            f"{system}://{user_name}:{password}@{host}:{port}/{database_name}")
-
-    except Exception as e:
-        print(f"Error: {e}. Failure to connect to database.")
-        return "Fail"
-
-    else:
-        print("Successful connection to database established.")
-        return engine
-
-
-def SQL_INSERT_STATEMENT_FROM_DATAFRAME(SOURCE, TARGET):
-    sql_texts = []
-    for index, row in SOURCE.iterrows():
-        sql_texts.append('INSERT INTO '+TARGET+' (' +
-                         str(', '.join(SOURCE.columns)) + ') VALUES ' + str(tuple(row.values)))
-    return sql_texts
-
-
-def SQL_commands_executor(commands: list):
-    conn = None
-    try:
-        # read the connection parameters
-        params = config()
-        # connect to the PostgreSQL server
-        conn = psycopg2.connect(**params)
-        cur = conn.cursor()
-        # create command one by one
-        for command in commands:
-            cur.execute(command)
-        # close communication with the PostgreSQL database server
-        cur.close()
-        # commit the changes
+        client = boto3.client('redshift', region_name='eu-west-1')
+        REDSHIFT_USER = "awsuser"
+        REDSHIFT_CLUSTER = "redshiftcluster-fbtitpjkbelw"
+        REDSHIFT_HOST = "redshiftcluster-fbtitpjkbelw.cnvqpqjunvdy.eu-west-1.redshift.amazonaws.com"
+        REDSHIFT_DATABASE = "team2db" # DATABASE NAME GOES HERE
+        
+        # Obtain Credentials
+        creds = client.get_cluster_credentials(
+        DbUser=REDSHIFT_USER,
+        DbName=REDSHIFT_DATABASE,
+        ClusterIdentifier=REDSHIFT_CLUSTER,
+        DurationSeconds=3600)
+        
+        # Create connection
+        conn = psycopg2.connect(
+        user=creds['DbUser'],
+        password=creds['DbPassword'],
+        host=REDSHIFT_HOST,
+        database=REDSHIFT_DATABASE,
+        port=5439
+        )
+        
+        # Create a cursor
+        cursor = conn.cursor()
+        
+        # Insert Data into table
+        cursor.executemany(f"""INSERT INTO {table} VALUES ({columns})""", Items)
+        print("Command executed successfully.")
         conn.commit()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
+        conn.close()
+        return "Success"
+    except Exception as e:
+            print(f"ERROR: {e}. StatusCode: 500")
+            
