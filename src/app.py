987b5c0c9ed2a71c23dba6bd77fac991bbd631b8
jaltpi <jenielthomas@gmail.com>
2021-09-17 09:37:09 +0100

    Put all code into a function called etl

--- src/app.py
+++ src/app.py
@@ -70,96 +70,98 @@ df = pd.read_csv("chesterfield_25-08-2021_09-00-00.csv", names=column) # Chester
 #df = pd.read_csv("longridge_25-08-2021_09-00-00.csv", names=column) # Longride
 #df = pd.read_csv("uppingham_25-08-2021_09-00-00.csv", names=column) # Uppingham
 
-new_elongated_raw_df = splitDataFrameList(df,"Order", ",")
+def etl(pandas_dataframe: object):
+    
+    new_elongated_raw_df = splitDataFrameList(df,"Order", ",")
 
-df[["date", "time"]] = df.Datetime.str.split(" ", expand=True,) #Split date and time
+    df[["date", "time"]] = df.Datetime.str.split(" ", expand=True,) #Split date and time
 
-long_raw_customers = new_elongated_raw_df["Customer"] # Combine this with transformed products for Basket df (clean with get/create customer id)
+    long_raw_customers = new_elongated_raw_df["Customer"] # Combine this with transformed products for Basket df (clean with get/create customer id)
 
-# Use to make below to make Orders df
-raw_dates = df["date"]
-raw_times = df["time"]
-raw_locations = df["Location"] # Clean This with get location
-raw_customers = df["Customer"] # Clean This with get/create customer id
-raw_total_price = df["Price"]
-raw_payment = df["Payment"]
+    # Use to make below to make Orders df
+    raw_dates = df["date"]
+    raw_times = df["time"]
+    raw_locations = df["Location"] # Clean This with get location
+    raw_customers = df["Customer"] # Clean This with get/create customer id
+    raw_total_price = df["Price"]
+    raw_payment = df["Payment"]
 
-# Use transform function on this
-raw_products = df["Order"]
+    # Use transform function on this
+    raw_products = df["Order"]
 
-# CLEAN LOCATIONS:
-database_location_id_tuple = query_db_for_location_tuples()
-#database_location_id_tuple = [(1,"Chesterfield"),(2, "London Soho"),(3, "London Camden"),(4, "Longridge"),(5, "Uppingham"),(6,"Isle of Wight")]
-dict_database_id = dict(database_location_id_tuple)
-database_id_keys = list(dict_database_id.keys()) # Create a list of keys
-database_id_values = list(dict_database_id.values()) # Create a list of values
-location_id = get_location_ID(raw_locations, database_id_keys, database_id_values)
+    # CLEAN LOCATIONS:
+    database_location_id_tuple = query_db_for_location_tuples()
+    #database_location_id_tuple = [(1,"Chesterfield"),(2, "London Soho"),(3, "London Camden"),(4, "Longridge"),(5, "Uppingham"),(6,"Isle of Wight")]
+    dict_database_id = dict(database_location_id_tuple)
+    database_id_keys = list(dict_database_id.keys()) # Create a list of keys
+    database_id_values = list(dict_database_id.values()) # Create a list of values
+    location_id = get_location_ID(raw_locations, database_id_keys, database_id_values)
 
-# CLEAN PRODUCTS:
-database_products_id_tuple = query_db_for_product_tuples()
-#database_products_id_tuple = []
-current_zipped_products_from_csv = transform_data(raw_products)
-all_current_cleaned_products_from_csv = zipped_items_into_list(current_zipped_products_from_csv)
-new_products_for_db = new_products_to_load(database_products_id_tuple, all_current_cleaned_products_from_csv) # compare db products and csv products
-load_data_redshift("products", "product_size,product_name,product_price", "%s,%s,%s", new_products_for_db)
+    # CLEAN PRODUCTS:
+    database_products_id_tuple = query_db_for_product_tuples()
+    #database_products_id_tuple = []
+    current_zipped_products_from_csv = transform_data(raw_products)
+    all_current_cleaned_products_from_csv = zipped_items_into_list(current_zipped_products_from_csv)
+    new_products_for_db = new_products_to_load(database_products_id_tuple, all_current_cleaned_products_from_csv) # compare db products and csv products
+    load_data_redshift("products", "product_size,product_name,product_price", "%s,%s,%s", new_products_for_db)
 
-#Use below to make database_products_df
-database_products_id_tuple = query_db_for_product_tuples()
-db_cleaned_product_id = [item[0] for item in database_products_id_tuple]
-db_cleaned_product_size = [item[1] for item in database_products_id_tuple]
-db_cleaned_product_name = [item[2] for item in database_products_id_tuple]
-db_cleaned_product_price = [item[3] for item in database_products_id_tuple]
+    #Use below to make database_products_df
+    database_products_id_tuple = query_db_for_product_tuples()
+    db_cleaned_product_id = [item[0] for item in database_products_id_tuple]
+    db_cleaned_product_size = [item[1] for item in database_products_id_tuple]
+    db_cleaned_product_name = [item[2] for item in database_products_id_tuple]
+    db_cleaned_product_price = [item[3] for item in database_products_id_tuple]
 
-# CLEAN CUSTOMERS:
-zipped_long_customer_id = create_customer_id(long_raw_customers)
-long_customer_id_tuple = zipped_items_into_list(zipped_long_customer_id)
-dict_customer_id = dict(long_customer_id_tuple)
-customer_id_keys = list(dict_customer_id.keys()) # Creates a list of keys
-customer_id_values = list(dict_customer_id.values()) # Creates a list of values
-long_customer_id = get_customer_id(long_raw_customers, customer_id_keys, customer_id_values)
-customer_id = get_customer_id(raw_customers, customer_id_keys, customer_id_values)
+    # CLEAN CUSTOMERS:
+    zipped_long_customer_id = create_customer_id(long_raw_customers)
+    long_customer_id_tuple = zipped_items_into_list(zipped_long_customer_id)
+    dict_customer_id = dict(long_customer_id_tuple)
+    customer_id_keys = list(dict_customer_id.keys()) # Creates a list of keys
+    customer_id_values = list(dict_customer_id.values()) # Creates a list of values
+    long_customer_id = get_customer_id(long_raw_customers, customer_id_keys, customer_id_values)
+    customer_id = get_customer_id(raw_customers, customer_id_keys, customer_id_values)
 
-# CLEANED ORDERS:
-orders_df = pd.DataFrame()
-orders_df["date"] = raw_dates
-orders_df["time"] = raw_times
-orders_df["location_id"] = location_id
-orders_df["customer_id"] = customer_id
-orders_df["price"] = raw_total_price
-orders_df["payment"] = raw_payment
-zipped_orders = zip_from_df_orders(orders_df)
-cleaned_orders = zipped_items_into_list(zipped_orders)
-load_data_redshift("orders","date,time,location_id,customer_id,price,payment", "%s,%s,%s,%s,%s,%s", cleaned_orders)
+    # CLEANED ORDERS:
+    orders_df = pd.DataFrame()
+    orders_df["date"] = raw_dates
+    orders_df["time"] = raw_times
+    orders_df["location_id"] = location_id
+    orders_df["customer_id"] = customer_id
+    orders_df["price"] = raw_total_price
+    orders_df["payment"] = raw_payment
+    zipped_orders = zip_from_df_orders(orders_df)
+    cleaned_orders = zipped_items_into_list(zipped_orders)
+    load_data_redshift("orders","date,time,location_id,customer_id,price,payment", "%s,%s,%s,%s,%s,%s", cleaned_orders)
 
-# CLEANED BASKET:
-temp_basket_df = pd.DataFrame()
-raw_size, raw_name, raw_price = zip(*all_current_cleaned_products_from_csv)
-temp_basket_df["customer_id"] = long_customer_id
-temp_basket_df["product_size"] = raw_size
-temp_basket_df["product_name"] = raw_name
-temp_basket_df["product_price"] = raw_price
+    # CLEANED BASKET:
+    temp_basket_df = pd.DataFrame()
+    raw_size, raw_name, raw_price = zip(*all_current_cleaned_products_from_csv)
+    temp_basket_df["customer_id"] = long_customer_id
+    temp_basket_df["product_size"] = raw_size
+    temp_basket_df["product_name"] = raw_name
+    temp_basket_df["product_price"] = raw_price
 
-products_df = pd.DataFrame()
-products_df["product_id"] = db_cleaned_product_id
-products_df["product_size"] = db_cleaned_product_size
-products_df["product_name"] = db_cleaned_product_name
-products_df["product_price"] = db_cleaned_product_price
+    products_df = pd.DataFrame()
+    products_df["product_id"] = db_cleaned_product_id
+    products_df["product_size"] = db_cleaned_product_size
+    products_df["product_name"] = db_cleaned_product_name
+    products_df["product_price"] = db_cleaned_product_price
 
-temp_basket_df = temp_basket_df.merge(products_df, on= ["product_size","product_name","product_price"], how="left")
+    temp_basket_df = temp_basket_df.merge(products_df, on= ["product_size","product_name","product_price"], how="left")
 
-basket_df = pd.DataFrame()
-basket_df["customer_id"] = temp_basket_df["customer_id"]
-basket_df["product_id"] = temp_basket_df["product_id"]
+    basket_df = pd.DataFrame()
+    basket_df["customer_id"] = temp_basket_df["customer_id"]
+    basket_df["product_id"] = temp_basket_df["product_id"]
 
-# Get recent Orders
-recent_orders_from_db = query_latest_entries("*", len(orders_df),"orders","order_id")
-recent_orders_df = pd.DataFrame(recent_orders_from_db, columns=["order_id","date","time","location_id", "customer_id", "price", "payment"])
+    # Get recent Orders
+    recent_orders_from_db = query_latest_entries("*", len(orders_df),"orders","order_id")
+    recent_orders_df = pd.DataFrame(recent_orders_from_db, columns=["order_id","date","time","location_id", "customer_id", "price", "payment"])
 
-basket_df = basket_df.merge(recent_orders_df, on = ["customer_id"], how="left")
-cleaned_basket_df = pd.DataFrame()
-cleaned_basket_df["order_id"] = basket_df["order_id"]
-cleaned_basket_df["customer_id"] = basket_df["customer_id"]
-cleaned_basket_df["product_id"] = basket_df["product_id"]
-zipped_basket = zip_from_basket_df(cleaned_basket_df)
-cleaned_basket = zipped_items_into_list(zipped_basket)
-load_data_redshift("basket", "order_id,customer_id,product_id", "%s,%s,%s", cleaned_basket)
+    basket_df = basket_df.merge(recent_orders_df, on = ["customer_id"], how="left")
+    cleaned_basket_df = pd.DataFrame()
+    cleaned_basket_df["order_id"] = basket_df["order_id"]
+    cleaned_basket_df["customer_id"] = basket_df["customer_id"]
+    cleaned_basket_df["product_id"] = basket_df["product_id"]
+    zipped_basket = zip_from_basket_df(cleaned_basket_df)
+    cleaned_basket = zipped_items_into_list(zipped_basket)
+    load_data_redshift("basket", "order_id,customer_id,product_id", "%s,%s,%s", cleaned_basket)
