caa270312d2ccf69507c2e532bb3fe03426b3e62
jaltpi <jenielthomas@gmail.com>
2021-09-17 11:49:30 +0100

    Removed any old unused code

--- src/Extract.py
+++ src/Extract.py
@@ -3,14 +3,6 @@ import boto3
 import io
 import psycopg2
 
-# def extract_csv_via_pandas(file_name, column_names: list):
-#     df = pd.read_csv(file_name, names=column_names)
-#     return df
-
-
-# column = ["DateTime", "Location", "Customer",
-#           "Order", "Payment_method", "Final_price", "PII"]
-# extract_csv_via_pandas('2021-02-23-isle-of-wight.csv', column)
 
 def extract_file() -> object:
     """This function reads a CSV File from an S3 Bucket and returns a dataframe containing the raw data"""
